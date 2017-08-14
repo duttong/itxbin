f03fd9d815917460e642b33047b46887fc72e9da
geoff <geoff.dutton@noaa.gov>
2017-08-14 11:03:29 -0600

    =multiprocess support for gcwerksimport.py

--- gcwerksimport.py
+++ gcwerksimport.py
@@ -4,112 +4,141 @@
 from glob import glob
 import argparse
 import os
+import gzip
+import shutil
+import multiprocessing
 from datetime import date
 
-cats_sites = ('brw', 'sum', 'nwr', 'mlo', 'smo', 'spo')
-path = '/hats/gc/'
+from catsbase import CATS_Chromatograph
+cats = CATS_Chromatograph()
 
+cats_sites = cats.sites
+today = date.today()
 
-def permission(site):
-    """ Used to prevent other hats member from running command on CATS data. """
-    if os.environ['LOGNAME'] != 'gdutton' and (site in cats_sites or site == 'all'):
-        print("Sorry, you don't have permission to run this command on site: {}".format(site))
-        quit()
-            
+class GCwerks_import():
 
-def import2gcwerks(site, chromsdir):
-    """ Importing routine.
+    def __init__(self, site, options):
+        self.site = site
+        self.options = options
+        self.path = '/hats/gc/'
+        if 'yyyy' not in self.options:
+            self.options['yyyy'] = today.year
+        yy = self.options['yyyy'][2:] if len(self.options['yyyy']) == 4 else self.options['yyyy']
+        self.chromsdir = self.path + self.site + '/' + yy + '/chroms_itx/'
+        self.permission()
+
+    def permission(self):
+        """ Used to prevent other hats member from running command on CATS data. """
+        if os.environ['LOGNAME'] != 'gdutton' and (self.site in cats_sites or self.site == 'all'):
+            print("Sorry, you don't have permission to run this command on site: {}".format(self.site))
+            quit()
+
+    def import2gcwerks(self):
+        """ Importing routine.
     
-        Import *.itx and *.gz (unless options.all is selected).  Once imported
-        compress *.itx files and rename with .Z extension.
+            Import *.itx and *.gz (unless options.all is selected).  Once imported
+            compress *.itx files and rename with .Z extension.
         
-        Excludes cal3 injections.  Will need a different routine for c1_c, c2_c and c3_c 
+            Excludes cal3 injections.  Will need a different routine for c1_c, c2_c and c3_c 
    
-        /hats/gc/itxbin/itx_import.py -g 2012nwr1672338.a1.itx.gz | /hats/gc/gcwerks-3/bin/chromatogram_import -gcdir /hats/gc/nwr
-    """
-    if site == 'bld1' or site == 'agc1':
-        files = glob(chromsdir+'*.itx') + glob(chromsdir+'*.itx.gz')
-        if options.all:
-            files += glob(chromsdir+'*.itx.Z')
-    elif site == 'std' or site == 'stdhp':
-        ''' Standard GCs '''
-        files = glob(chromsdir+'*.itx') + glob(chromsdir+'*.itx.gz')
-        if options.all:
-            files += glob(chromsdir+'*.itx.Z')
-    else:
-        ''' CATS sites '''
-        files = glob(chromsdir+'*.??.itx') + glob(chromsdir+'*.??.itx.gz')
-        if options.all:
-            files += glob(chromsdir+'*.??.itx.Z')
+            /hats/gc/itxbin/itx_import.py -g 2012nwr1672338.a1.itx.gz | /hats/gc/gcwerks-3/bin/chromatogram_import -gcdir /hats/gc/nwr
+        """
+        
+        if self.site == 'bld1' or self.site == 'agc1':
+            files = glob(self.chromsdir+'*.itx') + glob(self.chromsdir+'*.itx.gz')
+            if ('all', True) in self.options.items():
+                files += glob(self.chromsdir+'*.itx.Z')
+        elif self.site == 'std' or self.site == 'stdhp':
+            ''' Standard GCs '''
+            files = glob(self.chromsdir+'*.itx') + glob(self.chromsdir+'*.itx.gz')
+            if ('all', True) in self.options.items():
+                files += glob(self.chromsdir+'*.itx.Z')
+        else:
+            ''' CATS sites '''
+            files = glob(self.chromsdir+'*.??.itx') + glob(self.chromsdir+'*.??.itx.gz')
+            if ('all', True) in self.options.items():
+                files += glob(self.chromsdir+'*.??.itx.Z')
 
-    cmd = path + 'itxbin/itx_import.py '
-    werks = '{}gcwerks-3/bin/chromatogram_import -gcdir {}{} > /dev/null 2>&1'.format(path, path, site)
+        cmd = self.path + 'itxbin/itx_import.py '
+        werks = '{}gcwerks-3/bin/chromatogram_import -gcdir {}{} > /dev/null 2>&1'.format(self.path, self.path, self.site)
             
-    # apply spike filter before SG smoothing
-    if options.s:
-        cmd += '-s '
-        if options.v:
-            print('Applying single point spike filter')
+        # apply spike filter before SG smoothing
+        if ('s', True) in self.options.items():
+            cmd += '-s '
+            if ('v', True) in self.options.items():
+                print('Applying single point spike filter')
         
-    # apply wide spike filter?
-    if options.ws_start > 0:
-        cmd += '-W {} '.format(options.ws_start)
-        if options.v:
-            print('Applying wide spike filter starting at time={}'.format(options.ws_start))
+        # apply wide spike filter?
+        ws = self.options.get('ws_start')
+        if ws is not None and ws > 0:
+            cmd += '-W {} '.format(self.options.ws_start)
+            if ('v', True) in self.options.items():
+                print('Applying wide spike filter starting at time={}'.format(self.options.ws_start))
 
-    # apply Savitzky Golay smoothing    
-    if options.g:
-        cmd += '-g -gw {} -go {} '.format(options.SGwin, options.SGorder)
-        if options.v:
-            print('Applying Savitzky Golay smoothing: {}, {}'.format(options.SGwin, options.SGorder))
+        # apply Savitzky Golay smoothing    
+        if ('g', True) in self.options.items():
+            win = self.options.get('SGwin')
+            ord = self.options.get('SGorder')
+            cmd += '-g -gw {} -go {} '.format(win, ord)
+            if ('v', True) in self.options.items():
+                print('Applying Savitzky Golay smoothing: {}, {}'.format(win, ord))
 
-    if len(files) > 0:    
-        print('Importing {} files from {}.'.format(len(files), site))
-        for file in files:
-            if options.v:
-                print('   {}'.format(os.path.basename(file)))
-            os.system(cmd + " " + file + " | " + werks)
+        if len(files) > 0:    
+            print('Importing {} files from {}.'.format(len(files), self.site))
+            pool = multiprocessing.Pool()
+            for file in files:
+                pool.apply_async(self._process, args=(file, cmd, werks))
+            pool.close()
+            pool.join()
+
+    def _process(self, file, cmd, werks):
+        os.system(cmd + " " + file + " | " + werks)
+        if ('v', True) in self.options.items():
+            print('   {}'.format(os.path.basename(file)))
+                
+    def compress_to_Z(self, file):
+        with open(file, 'rb') as f_in, gzip.open(file+'.Z', 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+            os.remove(file)
         
-
-def rename(chromsdir):
-    """ Gzip *.itx files in the chroms_itx directory. 
+    def rename(self):
+        """ Gzip *.itx files in the chroms_itx directory. 
     
-        The files are also give the .Z suffix so that they will
-        not be re-imported.  Also rename any .gz files to .Z suffix.
-    """
-    files = glob(chromsdir + '*.itx')
-    if len(files) > 0:
-        print('Compressing *.itx files.')
-        for file in files:
-            os.system('/bin/gzip -fS .Z ' + file)
-            
-    files = glob(chromsdir + '*.gz')
-    if len(files) > 0:
-        print('Renameing *.gz files')
-        for file in files:
-            os.rename(file, file[:-2]+'Z')
+            The files are also give the .Z suffix so that they will
+            not be re-imported.  Also rename any .gz files to .Z suffix.
+        """
+                
+        """ compress .itx files to .Z files """
+        files = glob(self.chromsdir + '*.itx')
+        if len(files) > 0:
+            print('Compressing *.itx files.')
+            with multiprocessing.Pool() as pool:
+                for _ in pool.imap_unordered(self.compress_to_Z, files, chunksize=1):
+                    pass
+        
+        """ rename .gz file to .Z files """
+        files = glob(self.chromsdir + '*.gz')
+        if len(files) > 0:
+            print('Renameing *.gz files')
+            for file in files:
+                os.rename(file, file[:-2]+'Z')
     
         
-def main(site):
-    yy = options.yyyy[2:] if len(options.yyyy) == 4 else options.yyyy
-
-    chromsdir = path + site + '/' + yy + '/chroms_itx/'
-
-    import2gcwerks(site, chromsdir)
-    rename(chromsdir)
+    def main(self):
+        self.import2gcwerks()
+        self.rename()
     
-    # recreate index files for GCwerks -- not needed.  GCwerks adds to the index file.
-    os.system('/hats/gc/itxbin/quickindex.py ' + site)
-    #os.system('/hats/gc/gcwerks-3/bin/run-index -gcdir /hats/gc/' + site)
+        # recreate index files for GCwerks -- not needed.  GCwerks adds to the index file.
+        os.system('/hats/gc/itxbin/quickindex.py ' + self.site)
+        #os.system('/hats/gc/gcwerks-3/bin/run-index -gcdir /hats/gc/' + site)
 
-    # updates integration and mixing ratios
-    os.system('/hats/gc/gcwerks-3/bin/gcupdate -gcdir /hats/gc/' + site)
-    os.system('/hats/gc/gcwerks-3/bin/gccalc -gcdir /hats/gc/' + site + ' -1')
+        # updates integration and mixing ratios
+        os.system('/hats/gc/gcwerks-3/bin/gcupdate -gcdir /hats/gc/' + self.site)
+        os.system('/hats/gc/gcwerks-3/bin/gccalc -gcdir /hats/gc/' + self.site + ' -1')
 
 
 if __name__ == '__main__':
 
-    today = date.today()
     SGwin, SGorder = 61, 4      # Savitzky Golay default variables
     WSTART = -1                 # Wide spike filter start time
 
@@ -138,11 +167,11 @@ if __name__ == '__main__':
             
     options = opt.parse_args()
     
-    permission(options.site)
-    
     if options.site.lower() == 'all':
         for s in cats_sites:
+            werks = GCwerks_import(s, vars(options))
             print('Working on {}'.format(s))
-            main(s)
+            werks.main()
     else:
-        main(options.site)
+        werks = GCwerks_import(options.site, vars(options))
+        werks.main()
