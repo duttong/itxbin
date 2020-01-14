1b8be1991604767bf555a8fd894bb7a41862f8e4
Geoff <geoff.dutton@noaa.gov>
2020-01-14 14:37:33 -0700

    moved gcwerks.py to here

--- gcwerks.py
+++ gcwerks.py
@@ -1,1 +1,21 @@
-/home/hats/gdutton/bin/gcwerks.py
+#! /home/hats/gdutton/anaconda3/bin/python
+
+import argparse
+import os
+
+gcwerks = '/hats/gc/gcwerks-3/bin/gcwerks'
+cats_sites = ('brw','sum','nwr','mlo','smo','spo')
+
+if __name__ == '__main__':
+    goodsites = cats_sites + ('std','stdhp','bld1','agc1')
+
+    opt = argparse.ArgumentParser(
+        description="Used for launching GCwerks integration software."
+    )
+    opt.add_argument("site", default='all', help=f"Open GCwerks for {goodsites}")
+    options = opt.parse_args()
+
+    if options.site in goodsites:
+        os.system(f'{gcwerks} -gcdir /hats/gc/{options.site}')
+    else:
+        os.system(f'{gcwerks} -datadir /hats/gc')
