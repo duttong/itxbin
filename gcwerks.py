a20881d69bd8fa87448487406accc0001ce66b0f
pr1data <pr1data@andromeda.cmdl.noaa.gov>
2024-07-25 13:48:55 -0600

    added gui interface

--- gcwerks.py
+++ gcwerks.py
@@ -1,21 +1,73 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
-import argparse
-import os
+import sys
+import subprocess
+from PyQt5 import QtWidgets, QtGui, QtCore
 
-gcwerks = '/hats/gc/gcwerks-3/bin/gcwerks'
-cats_sites = ('brw','sum','nwr','mlo','smo','spo')
+GCWERKS_PATH = '/hats/gc/gcwerks-3/bin/gcwerks'
+CATS_SITES = {'brw', 'sum', 'nwr', 'mlo', 'smo', 'spo'}
+GOOD_SITES = sorted(CATS_SITES | {'std', 'stdhp', 'bld1', 'fe3', 'pr1'})
 
-if __name__ == '__main__':
-    goodsites = cats_sites + ('std','stdhp','bld1','fe3')
-
-    opt = argparse.ArgumentParser(
-        description="Used for launching GCwerks integration software."
-    )
-    opt.add_argument("site", default='all', help=f"Open GCwerks for {goodsites}")
-    options = opt.parse_args()
-
-    if options.site in goodsites:
-        os.system(f'{gcwerks} -gcdir /hats/gc/{options.site}')
+def launch_gcwerks(site):
+    if site == 'pr1':
+        command = [GCWERKS_PATH, '-gcdir', '/data/Perseus-1/']
     else:
-        os.system(f'{gcwerks} -datadir /hats/gc')
+        command = [GCWERKS_PATH, '-gcdir', f'/hats/gc/{site}']
+    try:
+        subprocess.run(command, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}", file=sys.stderr)
+
+def site_selection_ui():
+    app = QtWidgets.QApplication(sys.argv)
+    
+    window = QtWidgets.QWidget()
+    window.setWindowTitle("Select GCwerks Site")
+    window.setGeometry(100, 100, 300, 150)
+    window.setStyleSheet("background-color: mistyrose;")
+    
+    layout = QtWidgets.QVBoxLayout()
+    
+    label = QtWidgets.QLabel("Select a site:")
+    label.setFont(QtGui.QFont('Helvetica', 16))
+    layout.addWidget(label)
+    
+    site_combo = QtWidgets.QComboBox()
+    site_combo.setFont(QtGui.QFont('Helvetica', 16))
+    site_combo.addItems(GOOD_SITES)
+    site_combo.setStyleSheet("QComboBox { background-color: lightcoral; }")  # Setting background color to light red (lightcoral)
+    layout.addWidget(site_combo)
+    
+    def on_select():
+        selected_site = site_combo.currentText()
+        if selected_site:
+            window.close()
+            launch_gcwerks(selected_site)
+        else:
+            QtWidgets.QMessageBox.critical(window, "Selection Error", "Please select a valid site.")
+    
+    site_combo.currentIndexChanged.connect(on_select)
+    
+    window.setLayout(layout)
+    window.show()
+    
+    sys.exit(app.exec_())
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(
+        description="Launch GCwerks integration software."
+    )
+    parser.add_argument(
+        "site", nargs='?', choices=GOOD_SITES, default=None,
+        help=f"Open GCwerks for a specified site. Available sites: {', '.join(GOOD_SITES)}."
+    )
+    args = parser.parse_args()
+
+    if args.site:
+        launch_gcwerks(args.site)
+    else:
+        site_selection_ui()
+
+if __name__ == "__main__":
+    main()
