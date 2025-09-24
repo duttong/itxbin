86d9ba975ffa1c77326451b88a9432806dbb1b94
geoff <geoff.dutton@noaa.gov>
2025-09-24 09:26:28 -0600

    updated cron script to by python not shell

--- fe3_ingest.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-/hats/gc/itxbin/fe3_import.py > /dev/null 2>&1 
-/hats/gc/itxbin/fe3_export.py > /dev/null 2>&1
-#/hats/gc/itxbin/fe3_merge.py #> /dev/null 2>&1
-/hats/gc/itxbin/fe3_gcwerks2db.py
-#/hats/gc/itxbin/fe3_data2db.py
