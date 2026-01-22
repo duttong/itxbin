faa0b04cc886968ba4f1064b285ced0a10def5b5
geoff <geoff.dutton@noaa.gov>
2026-01-22 09:27:18 -0700

    added run-index to m4_all.sh workflow

--- m4_all.sh
+++ m4_all.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+# rebuild .run-index
+rm /hats/gc/m4/.run-index
+/hats/gc/gcwerks-3/bin/run-index -gcdir /hats/gc/m4
+
 /hats/gc/itxbin/m4_samplogs.py --all -i
 /hats/gc/itxbin/m4_gcwerks2db.py -x 2312
 /hats/gc/itxbin/m4_batch.py -p all -i -s 2312
